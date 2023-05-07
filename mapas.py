c51d7ec8e758bf8d78bafd95352c0786ade40018
Renilson <56858423+RenilsonA@users.noreply.github.com>
2023-05-07 20:33:15 -0300

    Update mapas.py

--- mapas.py
+++ mapas.py
@@ -3,7 +3,6 @@ import math as m
 def define_equacoes(mapa, nivel, x, y, u, funcoes = True):
     const = 0.65
     if(mapa == 1):
-        const = 0.35
         if(nivel == 1):
             const = 0.4
             l1 = - 5.2*x + 2.5*y + 3.2*u
@@ -26,29 +25,27 @@ def define_equacoes(mapa, nivel, x, y, u, funcoes = True):
             
         
     elif(mapa == 2):
-        const = 0.5
+        const = 0.2
         if(nivel == 1):
             l1 =   3.0*y + 1.0*u
             l2 = - 3.0*m.sin(8*x) - y
             
         elif(nivel == 2):
-            const = 0.2
             l1 = - 2.0*x + m.atan(3.0*x) + 2.0*y + 2.0*u
             l2 =   2.0*x - 2.0*y
             
         elif(nivel == 3):
-            const = 0.1
             l1 =   3.0*x*(0.5*x + 1.5*y) + 2.0*u
             l2 =   3.0*(x - y) + 1.0*u*u
             
         elif(nivel == 4):
-            const = 0.35
+            const = 0.05
             l1 = - 10.0*(x - 0.7)*(x + 0.7) + 5.0*u
             l2 = - 1.0*y + 2.0*u
                     
     elif(mapa == 3):
-        const = 0.45
         if(nivel == 1):
+            const = 0.2
             x = 10*x
             y = 10*y
             u  = 5*u
@@ -59,10 +56,11 @@ def define_equacoes(mapa, nivel, x, y, u, funcoes = True):
             else:
                 l1 = l2 - 3.0
             
-            l1 = l1/3
-            l2 = l2/3
+            l1 = l1/3.0
+            l2 = l2/3.0
         
         elif(nivel == 2):
+            const = 0.25
             l1 = -1.0*y
             l2 = -0.25*x
             if x > 0:
@@ -71,6 +69,7 @@ def define_equacoes(mapa, nivel, x, y, u, funcoes = True):
                 l2 = l2 - 2*u
                     
         elif(nivel == 3):
+            const = 0.25
             x = 10*x
             y = 10*y
             u  = 5*u
@@ -81,10 +80,11 @@ def define_equacoes(mapa, nivel, x, y, u, funcoes = True):
             else:
                 l2 = l2 - 4.0 - u
 
-            l1 = l1/4
-            l2 = l2/4
+            l1 = l1/4.0
+            l2 = l2/4.0
             
         elif(nivel == 4):
+            const = 0.3
             x = 10*x
             y = 10*y
             u  = 5*u
@@ -98,12 +98,12 @@ def define_equacoes(mapa, nivel, x, y, u, funcoes = True):
                 l1 = l1 - 0.1005*z*u 
                 l2 = l2 + 2.16*z*u
 
-            l1 = l1/6
-            l2 = l2/6
+            l1 = l1/6.0
+            l2 = l2/6.0
         
     elif(mapa == 4):
-        const = 0.7
         if(nivel == 1):
+            const = 0.2
             x = 10*x
             y = 10*y
             u  = 5*u
@@ -118,10 +118,11 @@ def define_equacoes(mapa, nivel, x, y, u, funcoes = True):
             else:
                 l2 = - 4.0
 
-            l1 = l1/5
-            l2 = l2/5
+            l1 = l1/5.0
+            l2 = l2/5.0
         
         elif(nivel == 2):
+            const = 0.35
             x = 10*x
             y = 10*y
             u  = 5*u
@@ -145,11 +146,11 @@ def define_equacoes(mapa, nivel, x, y, u, funcoes = True):
             if (l2 < -2.0):
                 l2 = -4.0
 
-            l1 = l1/5
-            l2 = l2/5
+            l1 = l1/5.0
+            l2 = l2/5.0
         
         elif(nivel == 3):
-            const = 0.1
+            const = 0.2
             x = 10*x
             y = 10*y
             u  = 5*u
@@ -172,25 +173,27 @@ def define_equacoes(mapa, nivel, x, y, u, funcoes = True):
                 l2 = 4.0
             if (l2 < -2.0):
                 l2 = -4.0
-            l1 = l1/5
-            l2 = l2/5
+                
+            l1 = l1/5.0
+            l2 = l2/5.0
         
         elif(nivel == 4):
+            const = 0.2
             x = 10*x
             y = 10*y
             u  = 5*u
             l1 = 1.0*y + 1.0*u
             l2 = -(9.8/1.0)*m.sin(x) - 1.0*y
-            if l1 > 0:
+            if l1 >= 0:
                 l1 = 2.0
             else:
                 l1 = -2.0
-            if l2 > 0:
+            if l2 >= 0:
                 l2 = 2.0
             else:
                 l2 = -2.0
-            l1 = l1/5
-            l2 = l2/5
+            l1 = l1/5.0
+            l2 = l2/5.0
             
     if funcoes:
         return (l1, l2)
