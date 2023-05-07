d6d4f98ce7a250813ff8aa55955c9206dde99aa5
Renilson <56858423+RenilsonA@users.noreply.github.com>
2023-05-06 22:05:44 -0300

    Add files via upload

---
+++ mapas.py
@@ -0,0 +1,197 @@
+import math as m
+
+def define_equacoes(mapa, nivel, x, y, u, funcoes = True):
+    const = 0.65
+    if(mapa == 1):
+        const = 0.35
+        if(nivel == 1):
+            const = 0.4
+            l1 = - 5.2*x + 2.5*y + 3.2*u
+            l2 = - 9.0*x - 0.8*y + 5.0*u
+             
+        elif(nivel == 2):
+            const = 0.5
+            l1 =   5.0*y
+            l2 = - 9.0*x - 0.9*y + 5.0*u
+            
+        elif(nivel == 3):
+            const = 0.25
+            l1 = - 1.2*x + 2.0*y
+            l2 =   3.6*x - 0.4*y - 5.0*u
+            
+        elif(nivel == 4):
+            const = 0.25
+            l1 =   1.0*x - 0.4*y + 0.6*u
+            l2 =   1.8*x + 0.2*y - 5.0*u
+            
+        
+    elif(mapa == 2):
+        const = 0.5
+        if(nivel == 1):
+            l1 =   3.0*y + 1.0*u
+            l2 = - 3.0*m.sin(8*x) - y
+            
+        elif(nivel == 2):
+            const = 0.2
+            l1 = - 2.0*x + m.atan(3.0*x) + 2.0*y + 2.0*u
+            l2 =   2.0*x - 2.0*y
+            
+        elif(nivel == 3):
+            const = 0.1
+            l1 =   3.0*x*(0.5*x + 1.5*y) + 2.0*u
+            l2 =   3.0*(x - y) + 1.0*u*u
+            
+        elif(nivel == 4):
+            const = 0.35
+            l1 = - 10.0*(x - 0.7)*(x + 0.7) + 5.0*u
+            l2 = - 1.0*y + 2.0*u
+                    
+    elif(mapa == 3):
+        const = 0.45
+        if(nivel == 1):
+            x = 10*x
+            y = 10*y
+            u  = 5*u
+            l1 = 1.0*y + 0.5*u
+            l2 = - 0.5*(x - y) + 0.8*u
+            if x > y:
+                l1 = l1 + 3
+            else:
+                l1 = l2 - 3.0
+            
+            l1 = l1/3
+            l2 = l2/3
+        
+        elif(nivel == 2):
+            l1 = -1.0*y
+            l2 = -0.25*x
+            if x > 0:
+                l2 = l2 + 2*u
+            else:
+                l2 = l2 - 2*u
+                    
+        elif(nivel == 3):
+            x = 10*x
+            y = 10*y
+            u  = 5*u
+            l1 = -1.0*y
+            l2 = 1.0*x
+            if x < y:
+                l2 = l2 + 4.0 + u
+            else:
+                l2 = l2 - 4.0 - u
+
+            l1 = l1/4
+            l2 = l2/4
+            
+        elif(nivel == 4):
+            x = 10*x
+            y = 10*y
+            u  = 5*u
+            z = 1.2759
+            l1 = 0.7578*x - 1.9796*y
+            l2 = 1.7454*x - 0.3350*y
+            if (-0.1582*x + 1.8467*y) > 0:
+                l1 = l1 + 0.1005*z*u 
+                l2 = l2 - 2.16*z*u
+            else:
+                l1 = l1 - 0.1005*z*u 
+                l2 = l2 + 2.16*z*u
+
+            l1 = l1/6
+            l2 = l2/6
+        
+    elif(mapa == 4):
+        const = 0.7
+        if(nivel == 1):
+            x = 10*x
+            y = 10*y
+            u  = 5*u
+            l1 = 1.0*y + 0.5*u
+            l2 = - 0.5*(x + y) + 0.8*u
+            if l1 > 0:
+                l1 = 4.0
+            else:
+                l1 = - 4.0
+            if l2 > 0:
+                l2 = 4.0
+            else:
+                l2 = - 4.0
+
+            l1 = l1/5
+            l2 = l2/5
+        
+        elif(nivel == 2):
+            x = 10*x
+            y = 10*y
+            u  = 5*u
+            l1 = - 1.0*y
+            l2 = 1.0*x + 1.0*u
+            if (l1 > 0.0) and (l1 < 2.0):
+                l1 = 2.0
+            if (l1 < 0.0) and (l1 > -2.0):
+                l1 = -2.0
+            if (l1 > 2.0):
+                l1 = 4.0
+            if (l1 < -2.0):
+                l1 = -4.0
+        
+            if (l2 > 0.0) and (l2 < 2.0):
+                l2 = 2.0
+            if (l2 < 0.0) and (l2 > -2.0):
+                l2 = -2.0
+            if (l2 > 2.0):
+                l2 = 4.0
+            if (l2 < -2.0):
+                l2 = -4.0
+
+            l1 = l1/5
+            l2 = l2/5
+        
+        elif(nivel == 3):
+            const = 0.1
+            x = 10*x
+            y = 10*y
+            u  = 5*u
+            l1 = 1.0*y
+            l2 = 1.0*x + 2.0*u
+            if (l1 > 0.0) and (l1 < 2.0):
+                l1 = 2.0
+            if (l1 < 0.0) and (l1 > -2.0):
+                l1 = -2.0
+            if (l1 > 2.0):
+                l1 = 4.0
+            if (l1 < -2.0):
+                l1 = -4.0
+        
+            if (l2 > 0.0) and (l2 < 2.0):
+                l2 = 2.0
+            if (l2 < 0.0) and (l2 > -2.0):
+                l2 = -2.0
+            if (l2 > 2.0):
+                l2 = 4.0
+            if (l2 < -2.0):
+                l2 = -4.0
+            l1 = l1/5
+            l2 = l2/5
+        
+        elif(nivel == 4):
+            x = 10*x
+            y = 10*y
+            u  = 5*u
+            l1 = 1.0*y + 1.0*u
+            l2 = -(9.8/1.0)*m.sin(x) - 1.0*y
+            if l1 > 0:
+                l1 = 2.0
+            else:
+                l1 = -2.0
+            if l2 > 0:
+                l2 = 2.0
+            else:
+                l2 = -2.0
+            l1 = l1/5
+            l2 = l2/5
+            
+    if funcoes:
+        return (l1, l2)
+    return const
---
+++ script.py
@@ -0,0 +1,158 @@
+from websocket_server import WebsocketServer
+import math as m
+import numpy as np
+import mapas
+import time
+
+porta = WebsocketServer(port=6660)
+pacote_anterior = []
+saida_anterior = 0.0
+saida = 0.0
+dt = 0.02
+
+def constrain(val, min_val, max_val):
+    return max(min(val, max_val), min_val)
+
+def computeBestDistance(targetX, targetY, positions):
+    minDistance = 100000
+    
+    for (i, position) in enumerate(positions):
+        currentDistance = ((position[0] - targetX) * (position[0] - targetX) + (position[1] - targetY)*(position[1] - targetY))
+        if currentDistance < minDistance:
+            minDistance = currentDistance
+
+    return minDistance
+
+def computePositions(x, y, U, land, level, testDepth, dt):
+    positions = []
+    uLength = len(U)
+    U.append(U[uLength-1])
+    p = [x, y]
+    for i in range(testDepth):
+        positions.append([p[0], p[1]])
+        uIndex = m.floor(uLength * i / testDepth)
+        u = U[uIndex]
+        xp = mapas.define_equacoes(land, level, p[0], p[1], u)
+        p[0] += xp[0]*dt
+        p[1] += xp[1]*dt
+    return positions
+
+def computaMelhorU(x, y, mapa, nivel, profundidade, dt, alvo_x, alvo_y):
+    melhorDist = np.inf     #minDistance
+    melhorI = 0             #bestControlSignal
+    for i in range(-100, 100, 1):
+        for j in range(-100, 100, 1):
+            valorUI = i / 100.0
+            valorUJ = j / 100.0
+            part = computePositions(x, y, [valorUI, valorUJ], mapa, nivel, profundidade, dt)
+            checarDist = computeBestDistance(alvo_x, alvo_y, part)
+            if(checarDist < melhorDist):
+                melhorDist = checarDist
+                melhorI = valorUI
+    return melhorI
+
+def checarU(mapa, nivel):
+    if(mapa == 2):
+        return 8
+    else:
+        return 10
+
+def computacaoInicial(x, y, mapa, nivel, profundidade, dt, alvo_x, alvo_y):
+    melhorDist = np.inf     #minDistance
+    melhorI = 0             #bestControlSignal
+    uMax = checarU(mapa, nivel)
+    for i in range(-uMax, uMax, 1):
+        for j in range(-uMax, uMax, 1):
+            valorUI = i / 10.0
+            valorUJ = j / 10.0
+            minDistance = np.inf
+            positions = []
+            vatorU = [valorUI, valorUJ]
+            uLength = len(vatorU)
+            vatorU.append(vatorU[uLength-1])
+            x1 = x
+            y1 = y
+            for k in range(profundidade):
+                #positions.append((x1, y1))
+                currentDistance = m.sqrt((x1 - alvo_x) * (x1 - alvo_x) + (y1 - alvo_y)*(y1 - alvo_y))
+                if currentDistance < minDistance:
+                    minDistance = currentDistance
+                uIndex = m.floor(uLength * k / profundidade)
+                u = vatorU[uIndex]
+                xp = mapas.define_equacoes(mapa, nivel, x1, y1, u)
+                x1 = x1 + xp[0]*dt
+                y1 = y1 + xp[1]*dt
+            #for l in range(len(positions)):
+            if(minDistance < melhorDist):
+                melhorDist = minDistance
+                melhorI = valorUI
+    return melhorI
+
+def repartir_mensagens(pacote):
+    vitorias = int(pacote[0])
+    mapa = int(pacote[1])
+    nivel = int(pacote[2])
+    jogador = pacote[3], pacote[4]
+    alvo = pacote[5], pacote[6]
+    caixas, caveiras = [], []
+    if(len(pacote) > 8):
+        caixas.append((pacote[7], pacote[8]))
+    if(len(pacote) > 10):
+        caixas.append((pacote[9], pacote[10]))
+    if(len(pacote) > 12):
+        caixas.append((pacote[11], pacote[12]))
+        for i in range(14, len(pacote), 1):
+            caveiras.append(pacote[i])
+    return vitorias, mapa, nivel, jogador, alvo, caixas, caveiras
+
+def cliente(cliente, servidor):
+    servidor.send_message(cliente, str(0))
+
+def receber_mensagem(cliente, servidor, pacote):
+    global pacote_anterior
+    global saida
+    global dt
+
+    values = [float(x) for x in pacote[:-1].split(",") if x.strip()]
+    vitorias, mapa, nivel, jogador, alvo, caixas, caveiras = repartir_mensagens(values)
+
+    if pacote_anterior != values:
+        const = mapas.define_equacoes(mapa, nivel, 0, 0, 0, False)
+        menor = (jogador[0] - alvo[0]) * (jogador[0] - alvo[0]) + (jogador[1] - alvo[1])*(jogador[1] - alvo[1])
+        Tx = alvo[0]
+        Ty = alvo[1]
+        limMax = 0.9
+        """if(abs(jogador[0]) > limMax or abs(jogador[1]) > limMax):
+            Tx = -jogador[0]
+            Ty = -jogador[1]"""
+        if(len(caixas) > 1 and mapa != 2):
+            if((jogador[0] - caixas[0][0]) * (jogador[0] - caixas[0][0]) + (jogador[1] - caixas[0][1])*(jogador[1] - caixas[0][1]) < menor):
+                menor = (jogador[0] - caixas[0][0]) * (jogador[0] - caixas[0][0]) + (jogador[1] - caixas[0][1])*(jogador[1] - caixas[0][1])
+                Tx = caixas[0][0]
+                Ty = caixas[0][1]
+            if(len(caixas) > 2):
+                if((jogador[0] - caixas[1][0]) * (jogador[0] - caixas[1][0]) + (jogador[1] - caixas[1][1])*(jogador[1] - caixas[1][1]) < menor):
+                    menor = (jogador[0] - caixas[1][0]) * (jogador[0] - caixas[1][0]) + (jogador[1] - caixas[1][1])*(jogador[1] - caixas[1][1])
+                    Tx = caixas[1][0]
+                    Ty = caixas[1][1]
+                if(len(caixas) > 3):
+                    if((jogador[0] - caixas[2][0]) * (jogador[0] - caixas[2][0]) + (jogador[1] - caixas[2][1])*(jogador[1] - caixas[2][1]) < menor):
+                        menor = (jogador[0] - caixas[2][0]) * (jogador[0] - caixas[2][0]) + (jogador[1] - caixas[2][1])*(jogador[1] - caixas[2][1])
+                        Tx = caixas[2][0]
+                        Ty = caixas[2][1]
+        saida = const*saida + (1 - const)*computacaoInicial(jogador[0], jogador[1], mapa, nivel, 20, dt, Tx, Ty)
+        if saida >= 1:
+            saida = 0.9
+        if saida <= -1:
+            saida = -0.9
+    else:
+        saida = 0
+
+    #saida_anterior = saida
+    pacote_anterior = values 
+    servidor.send_message(cliente, str(saida))
+    time.sleep(0.01)
+
+porta.set_fn_new_client(cliente)
+porta.set_fn_message_received(receber_mensagem)
+porta.run_forever()
